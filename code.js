54b8f737428fc95dc29457af816a6d3568811c7f
Abdullah Al Jobaer <surid.cfc95@gmail.com>
2019-04-02 00:04:36 +0600

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-document.getElementById("ctl00_MainContainer_ddlExpectedGrade").value = "A"
+document.getElementById("ctl00_MainContainer_ddlExpectedGrade").value = "A"; //Set your expected Grade Here.
 var Theory = "Theory";
 var Lab = "Lab";
 for(var i = 1; i <= 16; i++){//Change 16 to 12 if you are evaluating a Lab course
